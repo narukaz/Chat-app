1edc47fb7ce9d65ba0655b99edf21f7fafb590f7
narukaz <85193271+narukaz@users.noreply.github.com>
2024-08-20 02:27:43 +0530

    adding socket

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,54 @@
 const express = require('express');
 require('dotenv').config();
+const http = require('http')
+const {Server} = require('socket.io')
+const cors = require('cors');
+
 
 
 const app = express();
+const server = http.createServer(app);
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
 
-app.use(express.json())
+const io = new Server(server, 
+    {cors:{
+        origin: "http://localhost:5173",
+        methods:["GET", "POST"]
+    }});
+
+
+
+
+
+//scoket io code
+io.on("connection", (socket)=>{
+    
+
+     socket.on("connectTo", (data)=>{
+        socket.join(data);
+        console.log(data)
+     });
+     socket.on("send_message", (data)=>{
+        socket.to(data.id).emit("received_message", data);
+        console.log(data.message)
+     })
+     
+    
+     
+
+     
+})
 
 //enviromental variables
-const PORT = process.env.PORT ||3300;
 
 
-
-
-app.listen(PORT,()=>{console.log("server is up")})
+const PORT = process.env.PORT ||3000;
+server.listen(PORT,()=>{console.log("server is up")})
 
 app.post('/',(req,res)=>{
-    const data = req?.body
+    const data = req?.body;
     console.log(typeof data);
     if ((Object.keys(data).length === 0)) {
         return res.json({ error: "User data is missing or invalid" });
